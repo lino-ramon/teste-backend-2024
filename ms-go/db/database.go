8860cbe77623378c5f93c29ea7b331e4be58ce2b
Ramon Lino <pereira.ramonlino@gmail.com>
2024-07-03 19:56:10 -0300

    feat: Adicionado parametros de conexão do mongo através de ENVs para alterar conforme ambiente.

--- ms-go/db/database.go
+++ ms-go/db/database.go
@@ -3,19 +3,29 @@ package db
 import (
 	"context"
 	"fmt"
+	"ms-go/config/logger"
+	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var db *mongo.Collection
-var client *mongo.Client
+var (
+	db *mongo.Collection
+	client *mongo.Client
+	MONGO_HOST 		= os.Getenv("MONGO_HOST")
+	MAIN_COLLECTION = os.Getenv("MAIN_COLLECTION")
+	MONGO_DATABASE  = os.Getenv("MONGO_DATABASE")
+)
 
 func Connection() *mongo.Collection {
 	var err error
 
+	logger.Debug("Variaveis de ambiente: MONGO_HOST", MONGO_HOST)
+	logger.Debug("Variaveis de ambiente: MONGO_HOST", MONGO_DATABASE)
+	logger.Debug("Variaveis de ambiente: MONGO_HOST", MAIN_COLLECTION)
 	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb://mongodb:27017")
+	clientOptions := options.Client().ApplyURI("mongodb://" + MONGO_HOST +":27017")
 
 	// Connect to MongoDB
 	client, err = mongo.Connect(context.Background(), clientOptions)
@@ -32,7 +42,7 @@ func Connection() *mongo.Collection {
 	}
 
 	// Set the database and collection variables
-	db = client.Database("teste_backend").Collection("products")
+	db = client.Database(MONGO_DATABASE).Collection(MAIN_COLLECTION)
 
 	return db
 }
